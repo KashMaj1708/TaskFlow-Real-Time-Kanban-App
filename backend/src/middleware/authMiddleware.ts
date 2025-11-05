f2f8d9e3c79391279231559afdaf2cde74262618
KashMaj1708 <your.email@example.com>
2025-11-05 01:11:16 -0500

    log added for jwt secret error

--- backend/src/middleware/authMiddleware.ts
+++ backend/src/middleware/authMiddleware.ts
@@ -5,11 +5,13 @@ export const authMiddleware = (req: Request, res: Response, next: NextFunction) 
   const token = req.cookies.token;
 
   if (!token) {
+    console.log('JWT_SECRET IS NOT SET:', !!process.env.JWT_SECRET);
     return res.status(401).json({ success: false, message: 'Unauthorized: No token provided' });
   }
 
   try {
     // Verify the token and get the payload
+    console.log('JWT_SECRET IS SET:', !!process.env.JWT_SECRET);
     const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
     
     // Attach the payload (which should be { id: ..., username: ... }) to the request
