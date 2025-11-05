080081f4a5d7a01ab504be313b4c763f75698430
KashMaj1708 <your.email@example.com>
2025-11-05 01:24:51 -0500

    fix added for cors issue

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -99,8 +99,8 @@ export const login = async (req: Request, res: Response) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      secure: true,
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
